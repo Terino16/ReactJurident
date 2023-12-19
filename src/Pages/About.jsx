acd961c0af8ad6fa2d13f8e4c9ecab94d17636d6
Terino16 <123401184+Terino16@users.noreply.github.com>
2023-12-19 22:44:42 +0530

    adding pages routes

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,17 @@
-
+import '../Pages/About.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="about-content">
+    <img src="your-profile-picture.jpg" alt="Your Profile Picture"/>
+    <p>
+        Welcome to our About Us page! We are a passionate team dedicated to [your mission or purpose].
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vehicula ex id nisl consectetur,
+        in consequat quam tristique. Proin vel dolor ut velit tincidunt ullamcorper.
+    </p>
+</section>
   )
 }
 
