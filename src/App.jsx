6e94165e2a0a16774cf574d8517988599fe877f1
Terino16 <123401184+Terino16@users.noreply.github.com>
2023-12-19 22:33:13 +0530

    adding pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import './App.css'
 import Layout from './Layout'
 import Home from './Pages/Home'
 import {Routes,Route} from "react-router-dom";
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 function App() {
  return(
    <>
      <Layout>
      <Routes>
       <Route element={<Home/>} index></Route>
-        {/* <Route path="/about" element={<About/>}></Route> */}
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/contact" element={<Contact/>}></Route>
       </Routes>
-       
      </Layout>
    </> 
  )
