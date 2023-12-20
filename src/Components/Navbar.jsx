95a17d324df3dbe57f03a2ccce59dabc9d5529b9
Terino16 <123401184+Terino16@users.noreply.github.com>
2023-12-20 22:25:35 +0530

    adding navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,12 +2,25 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <nav className="navbar">
-    <div className="logo">Your Logo</div>
+    <div className="logo">
+      <img 
+      src="https://dentalist.netlify.app/static/media/logo.9cb018fc8926bbe6ba83.png"
+      alt="Logo"
+      width={150}
+      />
+    </div>
+    <div>
     <ul className="nav-links">
       <li><a href="/">Home</a></li>
       <li><a href="/about">About</a></li>
+      <li><a href="/service">Service</a></li>
+      <li><a href="/blog">Blog</a></li>
       <li><a href="/contact">Contact</a></li>
     </ul>
+    </div>
+    <div>
+      <button className='nav-button'>Book Appointment</button>
+    </div>
   </nav>
   )
 }
